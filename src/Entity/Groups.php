f80e54e2035489c1dd08958060f9e568394e59d5
Gregorie Reyes <reyesgregorie@gmail.com>
2020-07-30 11:44:42 +0200

    modification de la navbar, MàJ champ date dans table Groups

--- src/Entity/Groups.php
+++ src/Entity/Groups.php
@@ -33,7 +33,7 @@ class Groups
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="date", type="date", nullable=false)
+     * @ORM\Column(name="date", type="datetime", nullable=false)
      */
     private $date;
 
