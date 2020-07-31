fd18fccabba4b65e5315bd7ca4642cedb3d7a7f0
Abraham-jean-louis <malcomstille@gmail.com>
2020-07-31 17:36:35 +0200

    function edit profil en cours

--- src/Controller/ProfileUserController.php
+++ src/Controller/ProfileUserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Form\EditProfilType;
 use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\User;
 
 class ProfileUserController extends AbstractController
 {
@@ -17,5 +22,60 @@ class ProfileUserController extends AbstractController
         return $this->render('profile_user/index.html.twig', [
             'users' => $users,
         ]);
+    }
+
+    /**
+     * @Route("/admin/profil_user/edition{id}",defaults={"id":null})
+     * @param $id
+     * @param UsersRepository $usersRepository
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+
+    public function edit($id,
+                         UsersRepository $usersRepository,
+                         Request $request,EntityManagerInterface $entityManager
+    )
+    {
+
+        if (is_null($id))
+        {
+            $user = new Users();
+        }else
+            {
+                $user = $usersRepository->find($id);
+
+             }
+
+
+        $form = $this->redirectToRoute(EditProfilType::class,$user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted())
+        {
+            if ($form->isValid())
+            {
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_adminuser_index');
+            }
+
+        }
+
+        return  $this->render('profile_user/edit.html.twig',
+            [
+                'form' => $this->createView()
+
+            ]
+
+
+        );
+
+
+
     }
 }
