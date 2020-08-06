a9a9037423364b726a039f1db6a823927daab081
Gregorie Reyes <reyesgregorie@gmail.com>
2020-08-06 09:31:32 +0200

    test

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\AnswersRepository;
+use App\Repository\GroupsRepository;
 use App\Repository\QuestionsRepository;
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,15 +14,21 @@ class IndexController extends AbstractController
     /**
      * @Route("/")
      */
-    public function index(QuestionsRepository $questionsRepository, AnswersRepository $answersRepository)
+    public function index()
     {
+        //$group = $groupsRepository->find($id);
 
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
     }
 
-    public function search(){
-
-    }
+//    public function search(QuestionsRepository $questionsRepository){
+//
+//        $tag = $questionsRepository->findBy(['tag' => $tag]);
+//
+//        return $this->render('index/index.html.twig', [
+//            'tag' => $tag,
+//        ]);
+//    }
 }
