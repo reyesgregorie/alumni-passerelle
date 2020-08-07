f99aa6ffff483b00855cb00fa06b6df43afc67ad
Gregorie Reyes <reyesgregorie@gmail.com>
2020-08-07 17:29:45 +0200

    search bar en cours

--- assets/js/SearchBar.jsx
+++ assets/js/SearchBar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 class SearchBar extends React.Component{
+
+    state = { search: ''};
+
+    onChangeSearchText = (event) => {
+        console.log(event.target.value);
+        this.setState({search: event.target.value}); // event.target.value.toUpperCase()
+    }
+
     render(){
         return(
             <form className="pr-3" id="demo-2">
-                <input type="search" placeholder="Rechercher" />
+                <input type="search" placeholder="Rechercher" value={this.state.search} onChange={this.onChangeSearchText}/>
             </form>
         );
     }
