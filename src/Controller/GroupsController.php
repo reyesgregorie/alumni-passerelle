46880252241348e2cdb8a057e277f43c73bb97d9
Gregorie Reyes <reyesgregorie@gmail.com>
2020-08-11 17:04:26 +0200

    taff sur groupes+questions

--- src/Controller/GroupsController.php
+++ src/Controller/GroupsController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Questions;
+use App\Form\QuestionType;
 use App\Repository\GroupsRepository;
 use App\Repository\QuestionsRepository;
+use Doctrine\ORM\EntityManager;
+use \Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,7 +26,33 @@ class GroupsController extends AbstractController
         ]);
     }
 
-    public function list(){
+    /**
+     * @param $id
+     * @param QuestionsRepository $questionsRepository
+     * @param Request $request
+     * @param EntityManager $entityManager
+     * @Route("")
+     */
 
+    public function newQuestion($id, QuestionsRepository $questionsRepository, Request $request, EntityManager $entityManager){
+        if(is_null($id)){
+            $question = new Questions();
+        }
+        else{
+            $question = $questionsRepository->find($id);
+        }
+
+        $form = $this->createForm(QuestionType::class, $question);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                $entityManager->persist($question);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app');
+            }
+        }
+        return $this->render();
     }
 }
